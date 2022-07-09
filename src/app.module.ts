418b8188fce24e5d353c1002353afc04e55438eb
Torr7s <torresmiranda.rj@gmail.com>
2022-07-09 17:47:52 -0300

    feat(app): setup typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [process.env.TYPEORM_ENTITIES],
+      migrations: [process.env.TYPEORM_MIGRATIONS],
+      synchronize: true
+    })
+  ],
   controllers: [],
   providers: [],
 })
