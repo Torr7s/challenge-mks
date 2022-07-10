f8168ea3a98db3db3ecb00191c6b85d941f4afdb
Torr7s <torresmiranda.rj@gmail.com>
2022-07-10 17:34:07 -0300

    feat(books/interfaces): add list method

--- src/modules/books/domain/interfaces/books.interface.ts
+++ src/modules/books/domain/interfaces/books.interface.ts
@@ -3,8 +3,9 @@ import { iCreateBookProps, iUpdateBookProps } from '@types';
 import { BooksEntity } from '@modules/books/infra/typeorm/entities/books.entity';
 
 export interface iBooksRepository {
-  insertOne(data: iCreateBookProps): Promise<BooksEntity>;
   deleteOne(id: string): Promise<void>;
   findOne(id: string): Promise<BooksEntity>;
+  insertOne(data: iCreateBookProps): Promise<BooksEntity>;
+  list(): Promise<BooksEntity[]>;
   updateOne(data: iUpdateBookProps): Promise<BooksEntity>;
 }
