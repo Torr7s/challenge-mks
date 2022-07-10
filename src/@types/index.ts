ca6b6e43b955c33f2d3d1f6b7a864c9c5f2f6c97
Torr7s <torresmiranda.rj@gmail.com>
2022-07-10 17:33:52 -0300

    refactor(types): extend entity and omit data

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,5 +1,5 @@
 import { BooksEntity } from '@modules/books/infra/typeorm/entities/books.entity';
 
-export interface iCreateBookProps extends BooksEntity {};
+export interface iCreateBookProps extends Omit<BooksEntity, 'createdAt' | 'updatedAt'> {};
 
 export interface iUpdateBookProps extends Partial<iCreateBookProps> {};
