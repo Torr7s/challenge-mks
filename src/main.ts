5b023a35e94c59d7643eeb115de2dc0da8df44aa
Torr7s <torresmiranda.rj@gmail.com>
2022-07-10 18:37:50 -0300

    feat(app): enable validationpipe for data validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 (async (): Promise<void> => {
   const app: INestApplication = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }));
 
   await app.listen(8080);
 })();
