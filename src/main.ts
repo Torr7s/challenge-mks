83e0a59150a71999873cd386870a730b4cc761e5
Torr7s <torresmiranda.rj@gmail.com>
2022-07-10 20:44:29 -0300

    feat(main): setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -10,6 +11,16 @@ import { AppModule } from './app.module';
     whitelist: true,
     forbidNonWhitelisted: true
   }));
+
+  const config: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+    .setTitle('Swagger Documentation - MKS Backend Challenge')
+    .setVersion('3.4.0')
+    .addTag('Books')
+    .build()
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(8080);
 })();
