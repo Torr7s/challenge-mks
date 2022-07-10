e186a7c4369f9107241a892d14769b165a7243d8
Torr7s <torresmiranda.rj@gmail.com>
2022-07-10 13:18:45 -0300

    refactor(app): setup configmodule and typeormmodule with new options

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,17 @@
 import { Module } from '@nestjs/common';
+
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ormConfig } from '@config/typeorm/typeorm.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      entities: [process.env.TYPEORM_ENTITIES],
-      migrations: [process.env.TYPEORM_MIGRATIONS],
-      synchronize: true
-    })
-  ],
-  controllers: [],
-  providers: [],
+    ConfigModule.forRoot({ 
+      envFilePath: '.env.development',
+      isGlobal: true 
+    }),
+    TypeOrmModule.forRoot(ormConfig)
+  ]
 })
 export class AppModule {}
