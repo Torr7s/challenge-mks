2b3884753831a1ae34c85644e7ab26873c6264f2
Torr7s <torresmiranda.rj@gmail.com>
2022-07-10 18:43:04 -0300

    feat(books): create BooksModule and setup metadata

--- src/modules/books.module.ts
+++ src/modules/books.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { BooksEntity } from './books/infra/typeorm/entities/books.entity';
+import { BooksRepository } from './books/infra/typeorm/repositories/books.repository';
+
+import { BooksController } from './books/books.controller';
+import { BooksService } from './books/books.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       BooksEntity
     ])
+  ],
+  providers: [
+    BooksRepository,
+    BooksService
+  ],
+  controllers: [
+    BooksController
   ]
 })
 export class BooksModule {}
