3528d6601650eca2977d95b078b2b68a0dcc6baa
Torr7s <torresmiranda.rj@gmail.com>
2022-07-11 14:36:08 -0300

    tests(books): add final tests

--- src/modules/books/__tests__/books.service.spec.ts
+++ src/modules/books/__tests__/books.service.spec.ts
@@ -6,6 +6,12 @@ import { BooksEntity } from '../infra/typeorm/entities/books.entity';
 import { BooksRepository } from '../infra/typeorm/repositories/books.repository';
 import { BooksRepositoryInMemory } from '../infra/typeorm/repositories/inMemory/books.in-memo.repository';
 
+const booksList: BooksEntity[] = [
+  { name: 'test_name_1', author: 'test_author_1', pages: 1, url: 'test_url_1' },
+  { name: 'test_name_2', author: 'test_author_2', pages: 2, url: 'test_url_2' },
+  { name: 'test_name_3', author: 'test_author_3', pages: 3, url: 'test_url_3' },
+]
+
 describe('BooksService', (): void => {
   let service: BooksService;
 
@@ -27,26 +33,50 @@ describe('BooksService', (): void => {
     expect(service).toBeDefined();
   });
 
+  /* Delete a book */
+
   describe('deleteOne', (): void => {
     it('should not be able to delete a non-existent book', async (): Promise<void> => {
       await expect(service.deleteOne('123abcd'))
         .rejects
-        .toThrow('Invalid book ID provided.');
+        .toThrow('No book was found.');
     });
 
     it('should be able to delete a book', async (): Promise<void> => {
-      const book: BooksEntity = await service.insertOne({
+      const { id }: BooksEntity = await service.insertOne({
         name: 'test_name',
         author: 'test_author',
         pages: 1,
         url: 'test_url'
       });
 
-      expect(service.deleteOne(book.id)).toBeTruthy();
+      expect(service.deleteOne(id)).toBeTruthy();
     });
   });
 
+  /* Create a book */
+
   describe('insertOne', (): void => {
+    it('should not be able to insert a book with name already taken', async (): Promise<void> => {
+      await service.insertOne({
+        name: 'test_name',
+        author: 'test_author',
+        pages: 1,
+        url: 'test_url'
+      });
+
+      await expect(
+        service.insertOne({
+          name: 'test_name',
+          author: 'test_author',
+          pages: 1,
+          url: 'test_url'
+        })
+      )
+        .rejects
+        .toThrow('Book already exists.');
+    });
+
     it('should be able to insert a book', (): void => {
       expect(
         service.insertOne({
@@ -56,6 +86,119 @@ describe('BooksService', (): void => {
           url: 'test_url'
         })
       ).resolves.toHaveProperty('id');
+    });
+  });
+
+  /* Find a book by id */
+
+  describe('findById', (): void => {
+    it('should not be able to find a book by an invalid id', async (): Promise<void> => {
+      await expect(service.findById('123abcd'))
+        .rejects
+        .toThrow('No book was found.');
+    });
+
+    it('should be able to find a book by its id', async (): Promise<void> => {
+      const { id }: BooksEntity = await service.insertOne({
+        name: 'test_name',
+        author: 'test_author',
+        pages: 1,
+        url: 'test_url'
+      });
+
+      const book: BooksEntity = await service.findById(id);
+
+      expect(book).toHaveProperty('createdAt');
+    });
+  });
+
+  /* Find a book by name */
+
+  describe('findByName', (): void => {
+    it('should not be able to find a book by an unregistered name', async (): Promise<void> => {
+      await expect(service.findByName('**test_name'))
+        .rejects
+        .toThrow('No book was found.');
+    });
+
+    it('should be able to find a book by its name', async (): Promise<void> => {
+      const { name }: BooksEntity = await service.insertOne({
+        name: 'test_name',
+        author: 'test_author',
+        pages: 1,
+        url: 'test_url'
+      });
+
+      const book: BooksEntity = await service.findByName(name);
+
+      expect(book).toHaveProperty('createdAt');
+    });
+  });
+
+  /* List books */
+
+  describe('list', (): void => {
+    it('should not be able to list books for lack of records', async (): Promise<void> => {
+      await expect(service.list())
+        .rejects
+        .toThrow('No books were found.');
+    });
+
+    it('should be able to list all books', async (): Promise<void> => {
+      await service.insertOne({ name: 'test_name_1', author: 'test_author_1', pages: 1, url: 'test_url_1' });
+      await service.insertOne({ name: 'test_name_2', author: 'test_author_2', pages: 2, url: 'test_url_2' });
+      await service.insertOne({ name: 'test_name_3', author: 'test_author_3', pages: 3, url: 'test_url_3' });
+
+      const list: BooksEntity[] = await service.list();
+
+      expect(list).toBeDefined();
+      expect(list).toBeInstanceOf(Array);
+    });
+  });
+
+  /* Update a book */
+
+  describe('updateOne', (): void => {
+    it('should not be possible to update an unregistered book.', async (): Promise<void> => {
+      await expect(
+        service.updateOne({
+          name: 'test_name',
+          author: 'test_author',
+          pages: 1,
+          url: 'test_url'
+        })
+      )
+        .rejects
+        .toThrow('No book was found.');
+    });
+
+    it('should be possible to update a book', async (): Promise<void> => {
+      const book: BooksEntity = await service.insertOne({
+        name: 'test_name',
+        author: 'test_author',
+        pages: 1,
+        url: 'test_url'
+      });
+
+      const updatedBook: BooksEntity = {
+        id: book.id,
+        name: 'name_test',
+        author: 'author_test',
+        pages: 2,
+        url: 'url_test',
+      }
+
+      await expect(
+        service.updateOne({
+          id: book.id,
+          name: 'name_test',
+          author: 'author_test',
+          pages: 2,
+          url: 'url_test'
+        })
+      )
+        .resolves
+        .toEqual(updatedBook);
     });
   });
 });
