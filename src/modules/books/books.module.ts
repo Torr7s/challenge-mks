efb8a191c153596d6ff42c01c767107ba0e25021
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-30 20:21:09 -0300

    fix(modules/books): import new use cases

--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -4,8 +4,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BooksEntity } from './infra/typeorm/entities/books.entity';
 import { BooksRepository } from './infra/typeorm/repositories/books.repository';
 
-import { BooksController } from './books.controller';
-import { BooksService } from './books.service';
+import { CreateBookService } from './useCases/create/create-book.service';
+import { DeleteBookService } from './useCases/delete/delete-book.service';
+import { FindBookService } from './useCases/find/find-book.service';
+import { ListBooksService } from './useCases/list/list-books.service';
+import { UpdateBookService } from './useCases/update/update-book.service';
+
+import { CreateBookController } from './useCases/create/create-book.controller';
+import { DeleteBookController } from './useCases/delete/delete-book.controller';
+import { FindBookController } from './useCases/find/find-book.controller';
+import { ListBooksController } from './useCases/list/list-books.controller';
+import { UpdateBookController } from './useCases/update/update-book.controller';
 
 @Module({
   imports: [
@@ -15,10 +24,18 @@ import { BooksService } from './books.service';
   ],
   providers: [
     BooksRepository,
-    BooksService
+    CreateBookService,
+    DeleteBookService,
+    FindBookService,
+    ListBooksService,
+    UpdateBookService
   ],
   controllers: [
-    BooksController
+    CreateBookController,
+    DeleteBookController,
+    FindBookController,
+    ListBooksController,
+    UpdateBookController
   ]
 })
-export class BooksModule {}
+export class BooksModule {};
