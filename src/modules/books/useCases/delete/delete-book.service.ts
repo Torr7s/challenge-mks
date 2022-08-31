1b424919c80b1e34615ec2447bd58b5ad5108960
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-31 17:38:25 -0300

    feat(usecases/delete): add check if book doesnt exists

--- src/modules/books/useCases/delete/delete-book.service.ts
+++ src/modules/books/useCases/delete/delete-book.service.ts
@@ -3,13 +3,17 @@ import { Injectable } from '@nestjs/common';
 import { BooksEntity } from '@modules/books/infra/typeorm/entities/books.entity';
 import { BooksRepository } from '@modules/books/infra/typeorm/repositories/books.repository';
 
+import { BookNotFoundException } from '@shared/exceptions/books';
+
 @Injectable()
 export class DeleteBookService {
-  constructor(private booksRepository: BooksRepository ) {};
+  constructor(private booksRepository: BooksRepository) {};
 
   async perform(id: string): Promise<void> {
     const book: BooksEntity = await this.booksRepository.findById(id);
 
-    return this.booksRepository.deleteOne(book.id);
+    if (!book) throw new BookNotFoundException();
+
+    return this.booksRepository.deleteOne(id);
   }
 }
